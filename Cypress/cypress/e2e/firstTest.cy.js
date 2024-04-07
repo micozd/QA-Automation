2b18d29c73ac36775a4578bf624caae304b63445
micozd <ozdarskimichal@gmail.com>
2024-04-07 17:18:33 +0200

    initial commit

---
+++ Cypress/cypress/e2e/firstTest.cy.js
@@ -0,0 +1,25 @@
+describe("Nasz pierwszy blok testów", () => {
+    it("wizyta na stronie GoIT", () => {
+    cy.visit("https://www.edu.goit.global/account/login")
+    });
+
+    it("Wpisz email", () => {
+        cy.visit("https://www.edu.goit.global/account/login");
+        cy.get("#user_email").type("user888@gmail.com");
+        cy.get('#user_password').type("1234567890");
+        cy.get('.eckniwg2').click();
+    })
+
+    it("Wyloguj sie", () => {
+        cy.visit("https://www.edu.goit.global/account/login");
+        cy.get("#user_email").type("user888@gmail.com");
+        cy.get('#user_password').type("1234567890");
+        cy.get('.eckniwg2').click();
+        cy.get('#open-navigation-menu-mobile').click()
+        cy.get(':nth-child(12) > .next-bve2vl').scrollIntoView().should("be.visible")
+        cy.get(':nth-child(12) > .next-bve2vl').click()
+    })
+
+});
+
+
