37f9ce5445a21ce41b91a6bca02df618880c375e
micozd <ozdarskimichal@gmail.com>
2024-04-07 19:58:44 +0200

    nowe pliki po problemach z githubem

--- Cypress/e2e/firstTest.cy.js
+++ Cypress/e2e/firstTest.cy.js
@@ -1,25 +1,19 @@
-describe("Nasz pierwszy blok testów", () => {
-    it("wizyta na stronie GoIT", () => {
-    cy.visit("https://www.edu.goit.global/account/login")
-    });
-
-    it("Wpisz email", () => {
-        cy.visit("https://www.edu.goit.global/account/login");
-        cy.get("#user_email").type("user888@gmail.com");
-        cy.get('#user_password').type("1234567890");
-        cy.get('.eckniwg2').click();
-    })
-
-    it("Wyloguj sie", () => {
-        cy.visit("https://www.edu.goit.global/account/login");
-        cy.get("#user_email").type("user888@gmail.com");
-        cy.get('#user_password').type("1234567890");
-        cy.get('.eckniwg2').click();
+describe ("Pierwszy blok testów", () => {
+    //Test 1
+    it ("Wpisz e-mail i hasło dla user888", () => {
+        cy.visit("https://www.edu.goit.global/account/login")
+        cy.login("user888@gmail.com", "1234567890")
         cy.get('#open-navigation-menu-mobile').click()
         cy.get(':nth-child(12) > .next-bve2vl').scrollIntoView().should("be.visible")
         cy.get(':nth-child(12) > .next-bve2vl').click()
-    })
+    });
+    //Test 2
+    it ("Wpisz e-mail i hasło dla testowyqa", () => {
+        cy.visit("https://www.edu.goit.global/account/login")
+        cy.login("testowyqa@qa.team", "QA!automation-1")
+        cy.get('#open-navigation-menu-mobile').click()
+        cy.get(':nth-child(9) > .next-bve2vl').scrollIntoView().should("be.visible")
+        cy.get(':nth-child(9) > .next-bve2vl').click()
+    });
 
 });
-
-
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# QA-Automation
+# Projekty QA Automation
+
++ Aby uruchomić projekt zimportuj folder "QA-Automation" do VSC (File -> Open Folder...);
++ Otworz terminal;
++ Jeśli nie posiadasz Cypressa zainstaluj go poprzez komendy "npm init -y" oraz "npm i cypress --save-dev";
++ Aby uruchomić Cypressa uyj komendy "npx cypress open"
++ Po uruchomieniu wybierz opcję "E2E Testing" a następnie przeglądarkę, w której chcesz otworzyć projekt.
++ Następnie w folderze specs znajdź porządany plik;
+    - Praca domowa 1 - "login.cy.js"
